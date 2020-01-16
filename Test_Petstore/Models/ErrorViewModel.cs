056827277110fecf202e7095651800bd4d6688db
Walter Cubero L <wcubero@hotmail.com>
2020-01-15 22:54:23 -0600

    add sln for test

---
+++ Test_Petstore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Test_Petstore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
